7229674919e6b1dfa7ad2c8ca766985775e35409
sam <lad.sangram@gmail.com>
2020-10-09 22:43:39 +0100

    added page sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './sections/banner/banner.component';
+import { ContentComponent } from './sections/content/content.component';
+import { FooterComponent } from './sections/footer/footer.component';
+import { NavMenuComponent } from './sections/header/nav-menu/nav-menu.component';
+import {HeaderComponent} from './sections/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    ContentComponent,
+    FooterComponent,
+    NavMenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

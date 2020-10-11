74f3b834d55a0cfbd0bc0b8231fd3b83d271cbca
sam <lad.sangram@gmail.com>
2020-10-11 01:16:31 +0100

    completed other reasons tab

--- src/app/sections/content/other-reasons-tabs/other-reasons-tabs.component.ts
+++ src/app/sections/content/other-reasons-tabs/other-reasons-tabs.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./other-reasons-tabs.component.scss']
 })
 export class OtherReasonsTabsComponent implements OnInit {
-
+  selectedId = 1;
   constructor() { }
 
   ngOnInit(): void {

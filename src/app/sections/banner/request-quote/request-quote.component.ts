9fe3ce8d256e74de8776e48c9b2bedb14720c3c7
sam <lad.sangram@gmail.com>
2020-10-11 21:05:08 +0100

    implemented form validations

--- src/app/sections/banner/request-quote/request-quote.component.ts
+++ src/app/sections/banner/request-quote/request-quote.component.ts
@@ -10,13 +10,28 @@ export class RequestQuoteComponent implements OnInit {
   requestFormGroup: any;
 
   constructor() { }
-
+  get name(): FormControl {
+    return this.requestFormGroup.get('name');
+  }
+  get phone(): FormControl {
+    return this.requestFormGroup.get('phone');
+  }
+  get email(): FormControl {
+    return this.requestFormGroup.get('email');
+  }
   ngOnInit(): void {
     this.requestFormGroup = new FormGroup({
-      name: new FormControl('', Validators.required),
-      phone: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
     });
   }
 
+  submitForm = () => {
+    console.log(this.requestFormGroup.value);
+  }
+
+  isTouchedAndInvalid = (field: FormControl) => {
+   return (field.dirty || field.touched) && field.invalid && field.errors;
+  }
 }
